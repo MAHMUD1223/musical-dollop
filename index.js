7da4cb5133100e408e3bdbb3651f54aa9e32559e
Mahmud <117104429+MAHMUD1223@users.noreply.github.com>
2024-01-20 01:55:03 +0000

    adding react in frontend

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+app.use('/assets', express.static(__dirname+'/pages_react/dist/assets'))
 app.listen(port, () => {
     console.log(`server runing on ${port}`)
 })
 
 app.get('/', (req, res) => {
-    res.send("Hello from backend!")
+    res.sendfile(__dirname+'/pages_react/dist/index.html')
 })
 
 module.exports = app;
